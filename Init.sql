b95ab9a686995afae69620e2c9fb0de390a4af38
Ethan Montclair <montclair.ethan@gmail.com>
2020-08-20 22:45:08 -0600

    finalcommit

--- Init.sql
+++ Init.sql
@@ -38,5 +38,124 @@ JOIN playlist p ON pt.playlist_id = p.playlist_id;
 SELECT track.name, album.title
 FROM track
 JOIN album ON track.album_id = album.album_id
-JOIN genre ON genre.genre_id = track.album_id
+JOIN genre ON genre.genre_id = track.genre_id
 WHERE genre.name = 'Alternative & Punk';
+
+-- BLACK DIAMOND
+SELECT t.name, g.name, al.title, ar.name
+FROM track t
+JOIN genre g ON g.genre_id = t.genre_id
+JOIN album al ON al.album_id = t.album_id
+JOIN artist ar ON ar.artist_id = al.artist_id
+JOIN playlist_track pt ON pt.track_id = t.track_id
+JOIN playlist p ON p.playlist_id = pt.playlist_id
+WHERE p.name = 'Music';
+
+-- Practice nested queries
+-- 1.
+SELECT * FROM invoice
+WHERE invoice_id IN ( SELECT invoice_id FROM invoice_line WHERE unit_price > 0.99 );
+
+-- 2.
+SELECT * FROM playlist_track
+WHERE playlist_id IN ( SELECT playlist_id FROM playlist WHERE name = 'Music' );
+
+-- 3.
+SELECT name FROM track
+WHERE track_id IN ( SELECT track_id FROM playlist_track WHERE playlist_id = 5 );
+
+-- 4.
+SELECT * FROM track
+WHERE genre_id IN ( SELECT genre_id FROM genre WHERE name = 'Comedy' );
+
+-- 5.
+SELECT * FROM track
+WHERE album_id IN ( SELECT album_id FROM album WHERE title = 'Fireball' );
+
+-- 6.
+SELECT *FROM track
+WHERE album_id IN ( 
+  SELECT album_id FROM album WHERE artist_id IN ( 
+    SELECT artist_id FROM artist WHERE name = 'Queen'
+  )
+); 
+
+-- Practice updating rows
+-- 1.
+UPDATE customer
+SET fax = null
+WHERE fax IS NOT null;
+
+-- 2.
+UPDATE customer
+SET company = 'Self'
+WHERE company IS null;
+
+-- 3.
+UPDATE customer 
+SET last_name = 'Thompson' 
+WHERE first_name = 'Julia' AND last_name = 'Barnett';
+
+-- 4.
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl';
+
+-- 5.
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = ( SELECT genre_id FROM genre WHERE name = 'Metal' )
+AND composer IS null;
+
+
+-- Group by
+
+-- 1.
+SELECT COUNT(*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+GROUP BY g.name;
+
+-- 2.
+SELECT COUNT(*), g.name
+FROM track t
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name;
+
+-- 3.
+SELECT ar.name, COUNT(*)
+FROM album al
+JOIN artist ar ON ar.artist_id = al.artist_id
+GROUP BY ar.name;
+
+-- Use distinct
+
+-- 1.
+SELECT DISTINCT composer
+FROM track;
+
+-- 2.
+SELECT DISTINCT billing_postal_code
+FROM invoice;
+
+-- 3.
+SELECT DISTINCT company
+FROM customer;
+
+-- Delete rows 
+
+-- 1.
+DELETE 
+FROM practice_delete 
+WHERE type = 'bronze';
+
+-- 2.
+DELETE 
+FROM practice_delete 
+WHERE type = 'silver';
+
+-- 3.
+DELETE 
+FROM practice_delete 
+WHERE value = 150;
